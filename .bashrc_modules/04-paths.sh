4812266828e619b50d6fec4181c391710e31ad6b
Thomas Juhnke <tommy@van-tomas.de>
2017-04-06 23:21:02 +0200

    Add pip --user paths to PATH

--- .bashrc_modules/04-paths.sh
+++ .bashrc_modules/04-paths.sh
@@ -9,9 +9,20 @@ fi
 if [ -d ${HOME}/.composer/vendor/bin ]; then
   PATH="${HOME}/.composer/vendor/bin:${PATH}"
 fi
-# add ./bin to path
+
+python --version 1>/dev/null 2>&1 && {
+  _python_version="$(python --version 2>&1 | cut -d ' ' -f 2 | cut -d '.' -f 1,2)";
+  PATH="~/Library/Python/$_python_version/bin/:$PATH";
+}
+
+python3 --version 1>/dev/null 2>&1 && {
+  _python_version="$(python3 --version | cut -d ' ' -f 2 | cut -d '.' -f 1,2)";
+  PATH="~/Library/Python/$_python_version/bin/:$PATH";
+}
+
 PATH="./bin:$PATH"
 
 export CDPATH="$HOME/workspace:$HOME/playground:$CDPATH"
 export GOPATH="$HOME/workspace/golang"
+#export PYTHONPATH="$PYTHONPATH:$HOME/.local/lib/python$(python --version)/site-packages"
 
