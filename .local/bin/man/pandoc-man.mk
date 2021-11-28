f4fcb633cad6cdcc607fcada604d32575b78b227
Thomas Juhnke <tommy@van-tomas.de>
2021-11-28 21:21:16 +0100

    Switch to Pandoc for manpage generation

---
+++ .local/bin/man/pandoc-man.mk
@@ -0,0 +1,12 @@
+PANDOC ?= pandoc
+
+MANSECTION ?= 1
+
+MANPAGE.md = $(PANDOC) --standalone $(PANDOCFLAGS) --to man
+MANPAGE.html = $(PANDOC) --standalone $(PANDOCFLAGS) --to html
+
+%.$(MANSECTION): %.$(MANSECTION).md
+	$(MANPAGE.md) $< -o $@
+
+%.$(MANSECTION).html: %.$(MANSECTION).md
+	$(MANPAGE.html) $< -o $@
